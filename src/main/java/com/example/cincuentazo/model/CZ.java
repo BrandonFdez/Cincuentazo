d05009e47b044addc4e165dfcdf705bcaad61c20
brandonjimenez21 <jimenez.brandon@correounivalle.edu.co>
2024-12-12 18:58:31 -0500

    Se mejoro la jugabilidad, cartas anonimas para la IA y jugabilidad con la carta A

--- src/main/java/com/example/cincuentazo/model/CZ.java
+++ src/main/java/com/example/cincuentazo/model/CZ.java
@@ -174,7 +174,7 @@ public class CZ implements ICZ {
                   - 2-8 y 10 suman su valor.
                   - 9 no suma ni resta.
                   - J, Q, K restan 10.
-                  - A suma 1 o 11 según convenga.
+                  - A suma 1 o 10 según convenga.
                 • Turnos: Juega una carta o pasa si no puedes.
                 • Ganador: El último jugador en pie gana.
                 """);
